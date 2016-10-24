c73748309d8afae87ac0c1880ce97d9f475d50bf
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-23 21:20:44 -0700

    create function for random quote and added 1 more quote to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var quotes =
+var quotes = [
   'Blessed are the hearts that can bend; they shall never be broken. -Albert Camus',
   'Nothing behind me, everything ahead of me, as is ever so on the road. -Jack Kerouac',
   'Maybe ever\’body in the whole damn world is scared of each other. -John Steinbeck',
@@ -8,5 +8,11 @@ var quotes =
   'Anyone who ever gave you confidence, you owe them a lot. -Truman Capote',
   'Clocks slay time... time is dead as long as it is being clicked off by little wheels; only when the clock stops does time come to life. -William Faulkner',
   'The world where shadow is shadow and light is light, yin is yin and yang is yang. I\'m me and he\'s him. \'I am me and He is him: Autumn eve. -Haruki Murakami',
-  'All we ever see of stars are their old photographs. -Alan Moore'
+  'All we ever see of stars are their old photographs. -Alan Moore',
+  'Out, out, brief candle! Life\'s but a walking shadow, a poor player that struts and frets his hour upon the stage and then is heard no more. -William Shakespeare'
 ]
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
