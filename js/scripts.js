808e416bf429adb184e781393849ceedf15c6807
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-23 20:54:43 -0700

    add initial file, header and button, quote array

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var quotes =
+  'Blessed are the hearts that can bend; they shall never be broken. -Albert Camus',
+  'Nothing behind me, everything ahead of me, as is ever so on the road. -Jack Kerouac',
+  'Maybe ever\’body in the whole damn world is scared of each other. -John Steinbeck',
+  'Life is to be lived, not controlled; and humanity is won by continuing to play in face of certain defeat. -Ralph Ellison',
+  'Finally, from so little sleeping and so much reading, his brain dried up and he went completely out of his mind. -Miguel Cervantes',
+  'No one forgets the truth; they just get better at lying. -Richard Yates',
+  'Anyone who ever gave you confidence, you owe them a lot. -Truman Capote',
+  'Clocks slay time... time is dead as long as it is being clicked off by little wheels; only when the clock stops does time come to life. -William Faulkner',
+  'The world where shadow is shadow and light is light, yin is yin and yang is yang. I\'m me and he\'s him. \'I am me and He is him: Autumn eve. -Haruki Murakami',
+  'All we ever see of stars are their old photographs. -Alan Moore'
+]
